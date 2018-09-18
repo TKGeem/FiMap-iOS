b54d52bbf963ad2c0e7af8673aa03b83ce0f14e4
ammYou <ammyou.nf@gmail.com>
2018-09-19 00:19:07 +0900

    Create new empty project.

---
+++ FiMap/FiMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FiMap
+//
+//  Created by AmamiYou on 2018/09/19.
+//  Copyright © 2018 ammYou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
